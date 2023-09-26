dbe203928cffe701c6bc10b3d1d25e4a3270e47d
Ayomide <primedaug@outlook.com>
2023-09-26 23:17:52 +0100

    My Assignment Commit

--- src/ABAYOMI_BOOKSTORE/Book.java
+++ src/ABAYOMI_BOOKSTORE/Book.java
@@ -14,33 +14,10 @@ public class Book {
             this.price = price;
         }
 
-        public String getTitle() {
-            return title;
-        }
 
-        public void setTitle(String title) {
-            this.title = title;
-        }
-
-        public Genre getGenre() {
-            return genre;
-        }
-
-        public void setGenre(Genre genre) {
-            this.genre = genre;
-        }
-
-        public double getPrice() {
-            return price;
-        }
-
-        public void setPrice(double price) {
-            this.price = price;
-        }
-        // Getters and setters for attributes.
-        // This will be needed by the customers to ascertain attributes of the books they require.
 
     public static void main(String[] args) {
+
 
         }
 
--- src/ABAYOMI_BOOKSTORE/Bookstore.java
+++ src/ABAYOMI_BOOKSTORE/Bookstore.java
@@ -1,21 +1,41 @@
 package ABAYOMI_BOOKSTORE;
 
+import com.sun.jdi.PathSearchingVirtualMachine;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class Bookstore {
+public class Bookstore extends Book{
 
-        private List<Book> bookShelf = new ArrayList<>(50);
-        //creating an array list of books on the bookshelf.
+        private static List<Book> bookShelf = new ArrayList<>(50);
 
-        public void addBook(Book book) {
-            bookShelf.add(book);
+    public Bookstore(String title, Genre genre, double price) {
+        super(title, genre, price);
+    }
+    //creating an array list of books on the bookshelf.
+
+    public void addBook(Book book) {
+        bookShelf.add(book);
+        Book book001 = new Book("Harry Potter", Genre.FICTION, 3550.0);
+        Book book002 = new Book("Macmillan English 2", Genre.LANGUAGE, 2575.5);
+        Book book003 = new Book("Essential Economics 3", Genre.FINANCE, 5525.9);
+
+        bookShelf.add(book001);
+        bookShelf.add(book002);
+        bookShelf.add(book003);
+
+
             //This method enables the storekeeper to add books to the bookshelf.
             //It impacts directly in the Class Book as the class has been called directly in the method.
         }
 
-        public List<Book> AllBooks() {
+        public static List<Book> AllBooks() {
+
             return bookShelf;
         }
+
+    public static void main(String[] args) {
+        System.out.println(bookShelf);
+    }
 
 }
